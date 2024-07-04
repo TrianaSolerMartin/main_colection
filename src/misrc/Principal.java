bb40a499a31d4153ce32de3a371ca3bdceafa7d0
TrianaSolerMartin <triana287@gmail.com>
2024-07-04 09:45:01 +0200

    treset to array

--- src/misrc/Principal.java
+++ src/misrc/Principal.java
@@ -3,6 +3,8 @@ package misrc;
 
 // @author Administrator
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -15,6 +17,7 @@ public class Principal {
     //CREAR UNA COLECCION TREESET
     
     public static void ejemplo1 (){
+
     Set<Integer> enteros_ts = new TreeSet<>();
     enteros_ts.add(2);
     enteros_ts.add(1);
@@ -22,8 +25,20 @@ public class Principal {
     enteros_ts.add(4);
     enteros_ts.add(4);
     
+ //filtra y elimina los duplicados ademas de ordenado alfabetica o numericamente
     for(Integer elemento: enteros_ts){
-        System.out.print(elemento + "");
+        System.out.print(elemento + "a ");
+    }
+
+    ////recorre el treeset elemento por elemento 
+    List<Integer> enteros_al =new ArrayList<>(enteros_ts);
+    
+    for (Integer elemento: enteros_al){
+        System.out.print(elemento +"b ");
+    }
+    
+    for (int i=0; i<enteros_ts.size(); i++){
+    System.out.print(enteros_al.get(i)+"c ");
     }
     }
 
